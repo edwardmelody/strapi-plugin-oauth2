47c7db14e60dd5604904eebb11582fb4ace3c9c5
Sorasak Siangsanan <siangsanan.sorasak@gmail.com>
2025-12-10 10:40:52 +0700

    Refactor code structure for improved readability and maintainability

--- dist/_chunks/en-DbkiNMft.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, Symbol.toStringTag, { value: "Module" });
-const en = {
-  "oauth2.plugin.name": "OAuth 2.0",
-  "oauth2.plugin.description": "OAuth 2.0 plugin for Strapi"
-};
-exports.default = en;
